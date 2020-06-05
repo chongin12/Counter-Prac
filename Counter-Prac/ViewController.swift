67d0a269ad8decb764b89f723f3bd243981ae071
정종인 <chongin12@naver.com>
2020-06-05 15:29:17 +0900

    Initial Commit

---
+++ Counter-Prac/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Counter-Prac
+//
+//  Created by 정종인 on 2020/06/05.
+//  Copyright © 2020 swmaestro10th. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
