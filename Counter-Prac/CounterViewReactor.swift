86423bf5945d709de11235edeb700eead4fdaaab
정종인 <chongin12@naver.com>
2020-06-07 01:23:09 +0900

    reactor 추가

---
+++ Counter-Prac/CounterViewReactor.swift
@@ -0,0 +1,65 @@
+//
+//  CounterViewReactor.swift
+//  Counter-Prac
+//
+//  Created by 정종인 on 2020/06/06.
+//  Copyright © 2020 swmaestro10th. All rights reserved.
+//
+
+import Foundation
+import ReactorKit
+
+class CounterViewReactor: Reactor {
+    
+    enum Action {
+        case increase
+        case decrease
+    }
+    
+    enum Mutation {
+        case increaseValue
+        case decreaseValue
+        case isLoading(Bool)
+    }
+    
+    struct State {
+        var value: Int
+        var isLoading: Bool
+    }
+    
+    var initialState: State
+    
+    init() {
+        self.initialState = State(value: 0, isLoading: false)
+    }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .increase:
+            return Observable.concat([
+                Observable.just(Mutation.isLoading(true)),
+                Observable.just(Mutation.increaseValue),
+                Observable.just(Mutation.isLoading(false))
+            ])
+        case .decrease:
+            return Observable.concat([
+                Observable.just(Mutation.isLoading(true)),
+                Observable.just(Mutation.decreaseValue),
+                Observable.just(Mutation.isLoading(false))
+            ])
+        }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var state = state
+        switch mutation {
+        case .increaseValue:
+            state.value += 1
+        case .decreaseValue:
+            state.value -= 1
+        case .isLoading(let isLoading):
+            state.isLoading = isLoading
+        }
+        return state
+    }
+}
